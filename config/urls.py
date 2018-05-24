2254da8802a48dc4364e84d380e3c1fccd358d6a
Andrew Ang <ang.kunanit@gmail.com>
2018-05-24 12:00:10 -0400

    Add health check endpoint

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('health/', views.health),
     path('recommend/', include('apps.recommend.urls')),
+    path('admin/', admin.site.urls),
 ]
