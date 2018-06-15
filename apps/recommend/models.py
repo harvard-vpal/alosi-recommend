cef8cb1f02a623c3f249d8d2ee9bacf9dfea22cd
Andrew Ang <ang.kunanit@gmail.com>
2018-06-15 03:05:59 -0400

    unique identifier fields on choice and collection

--- apps/recommend/models.py
+++ apps/recommend/models.py
@@ -7,11 +7,12 @@ from django.db.models import fields
 
 class Collection(models.Model):
     name = fields.CharField(max_length=200)
+    collection_id = fields.CharField(max_length=200, unique=True)
 
 
 class Choice(models.Model):
     name = fields.CharField(max_length=200)
-    key = fields.CharField(max_length=200)
+    key = fields.CharField(max_length=200, unique=True)
     description = fields.TextField(default='', blank=True)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     image_url = fields.URLField(blank=True, null=True)
